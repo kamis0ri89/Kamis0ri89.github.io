9b3dc1aa17d7b321a5ed9dceb658625dc9082845
kamis0ri89 <kamis0ri89@gmail.com>
2015-12-15 14:17:49 -0500

    asdf
    asdf

--- iframe.js
+++ iframe.js
@@ -1,23 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-        var node = document.getElementById("stream");
+
+					var node = document.getElementById("stream");
  
-        var channels = ["channel1", "channel2", "channel3"];
-        var request = new XMLHttpRequest();
-        request.addEventListener("load", function (e) {
-                var data = JSON.parse(request.responseText);
-                var live = data.streams.map(function (stream) {return stream.channel.name});
+						var channels = ["Anniebot", "Braxas19", "Crizzy1994", "DreamKazper", "Jakehounds13", "Jannso", "Kamis0ri89", "Mom92", "Painlessdeath22", "Raynaud05", "RemarkableMe", "Souvie", "vortexware"];
+						var request = new XMLHttpRequest();
+							request.addEventListener("load", function (e) {
+						var data = JSON.parse(request.responseText);
+						var live = data.streams.map(function (stream) {return stream.channel.name});
        
-                var random, iframe;
-                if (live.length > 0) {
-                        random = live[Math.floor(Math.random() * live.length)];
+						var random, iframe;
+							if (live.length > 0) {
+							random = live[Math.floor(Math.random() * live.length)];
  
                         var iframe = document.createElement("iframe");
-                        iframe.src = "http://www.twitch.tv/" + random + "/embed";
-                        node.appendChild(iframe);
-                        node.removeAttribute("hidden");
-                }
-        });
+							iframe.src = "http://www.twitch.tv/" + random + "/embed";
+							node.appendChild(iframe);
+							node.removeAttribute("hidden");
+							iframe.width = "1080";
+							iframe.height = "520";
+						
+						}
+						});
  
-        request.open("GET", "https://api.twitch.tv/kraken/streams?channel=" + channels.join(","));
-        request.send();
-});
+						request.open("GET", "https://api.twitch.tv/kraken/streams?channel=" + channels.join(","));
+						request.send();
+						});
